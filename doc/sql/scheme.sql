6c4ecb2046d970d5d08511b722a502dafd892ff9
DIYILIU <572772828@qq.com>
2018-05-19 23:26:31 +0800

    添加错误处理

--- doc/sql/scheme.sql
+++ doc/sql/scheme.sql
@@ -1,100 +1,19 @@
-drop table if exists rel_user_role;
+/*
+Navicat MySQL Data Transfer
 
-drop table if exists sys_asset;
+Source Server         : 阿里云
+Source Server Version : 50173
+Source Host           : 106.15.89.145:3306
+Source Database       : admin3
 
-drop table if exists sys_privilege;
+Target Server Type    : MYSQL
+Target Server Version : 50173
+File Encoding         : 65001
 
-drop table if exists sys_role;
+Date: 2018-05-19 23:20:57
+*/
 
-drop table if exists sys_user;
-
-/*==============================================================*/
-/* Table: rel_user_role                                         */
-/*==============================================================*/
-create table rel_user_role
-(
-   id                   int not null auto_increment,
-   user_id              int,
-   role_id              int,
-   primary key (id)
-);
-
-/*==============================================================*/
-/* Table: sys_asset                                             */
-/*==============================================================*/
-create table sys_asset
-(
-   id                   int not null auto_increment,
-   name                 varchar(50) comment '资源名称',
-   code                 varchar(50) comment '资源代码',
-   pid                  int comment '父ID',
-   pids                 varchar(80) comment '父节组ID',
-   type                 varchar(50) comment '类型',
-   controller           varchar(100)  comment '控制器',
-   view                 varchar(100)  comment '视图',
-   icon_css             varchar(100) comment '图标css',
-   is_menu              int comment '是否菜单',
-   sort                 int comment '排序',
-   primary key (id)
-);
-
-alter table sys_asset comment '系统资源';
-
-/*==============================================================*/
-/* Table: sys_privilege                                         */
-/*==============================================================*/
-create table sys_privilege
-(
-   id                   int not null auto_increment,
-   master               varchar(50),
-   master_value         varchar(100),
-   access               varchar(50),
-   access_value         varchar(200),
-   permission           varchar(50),
-   comment              varchar(200),
-   primary key (id)
-);
-
-alter table sys_privilege comment '权限';
-
-/*==============================================================*/
-/* Table: sys_role                                              */
-/*==============================================================*/
-create table sys_role
-(
-   id                   int not null auto_increment,
-   pid                  int comment '父ID',
-   name                 varchar(50) comment '角色名称',
-   code                 varchar(30) comment '角色代码',
-   comment              varchar(100) comment '角色描述',
-   create_user          varchar(50) comment '创建人',
-   create_time          datetime comment '创建时间',
-   primary key (id)
-);
-
-/*==============================================================*/
-/* Table: sys_user                                              */
-/*==============================================================*/
-create table sys_user
-(
-   id                   int not null auto_increment,
-   username             varchar(50) comment '用户名',
-   password             varchar(80)  comment '登录密码',
-   salt                 varchar(50) comment '盐',
-   name                 varchar(20)  comment '真实姓名',
-   email                varchar(50) comment '邮箱',
-   tel                  varchar(15) comment '联系电话',
-   org_id               int comment '用户所属机构',
-   create_time          datetime comment '创建时间',
-   create_user          varchar(50)  comment '创建人',
-   status               int comment '用户状态',
-   expire_time          datetime comment '过期时间',
-   login_count          int comment '登陆次数',
-   last_login_ip        varchar(20) comment '最后登陆IP',
-   last_login_time      datetime comment '最后登陆时间',
-   primary key (id)
-);
-
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for guide_site
@@ -107,9 +26,10 @@ CREATE TABLE `guide_site` (
   `IMAGE` varchar(200) DEFAULT NULL,
   `TYPE` int(11) DEFAULT NULL,
   `COMMENT` varchar(100) DEFAULT NULL,
-  `TOP` int(11) DEFAULT NULL,
+  `SORT` int(11) DEFAULT NULL,
+  `CREATE_TIME` datetime DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8 COMMENT='网站地址';
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8 COMMENT='网站地址';
 
 -- ----------------------------
 -- Table structure for guide_type
@@ -119,6 +39,89 @@ CREATE TABLE `guide_type` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `CODE` varchar(5) DEFAULT NULL,
   `NAME` varchar(20) DEFAULT NULL,
-  `TOP` int(11) DEFAULT NULL,
+  `SORT` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COMMENT='网址类型';
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COMMENT='网址类型';
+
+-- ----------------------------
+-- Table structure for rel_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `rel_user_role`;
+CREATE TABLE `rel_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sys_asset
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_asset`;
+CREATE TABLE `sys_asset` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL COMMENT '资源名称',
+  `code` varchar(50) DEFAULT NULL COMMENT '资源代码',
+  `pid` int(11) DEFAULT NULL COMMENT '父ID',
+  `pids` varchar(80) DEFAULT NULL COMMENT '父节组ID',
+  `type` varchar(50) DEFAULT NULL COMMENT '类型',
+  `controller` varchar(100) DEFAULT NULL COMMENT '控制器',
+  `view` varchar(100) DEFAULT NULL COMMENT '视图',
+  `icon_css` varchar(100) DEFAULT NULL COMMENT '图标css',
+  `is_menu` int(11) DEFAULT NULL COMMENT '是否菜单',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='系统资源';
+
+-- ----------------------------
+-- Table structure for sys_privilege
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_privilege`;
+CREATE TABLE `sys_privilege` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `master` varchar(50) DEFAULT NULL,
+  `master_value` varchar(100) DEFAULT NULL,
+  `access` varchar(50) DEFAULT NULL,
+  `access_value` varchar(200) DEFAULT NULL,
+  `permission` varchar(50) DEFAULT NULL,
+  `comment` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=156 DEFAULT CHARSET=utf8 COMMENT='权限';
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL COMMENT '父ID',
+  `name` varchar(50) DEFAULT NULL COMMENT '角色名称',
+  `code` varchar(30) DEFAULT NULL COMMENT '角色代码',
+  `comment` varchar(100) DEFAULT NULL COMMENT '角色描述',
+  `create_user` varchar(50) DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(80) DEFAULT NULL COMMENT '登录密码',
+  `salt` varchar(50) DEFAULT NULL COMMENT '盐',
+  `name` varchar(20) DEFAULT NULL COMMENT '真实姓名',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `tel` varchar(15) DEFAULT NULL COMMENT '联系电话',
+  `org_id` int(11) DEFAULT NULL COMMENT '用户所属机构',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_user` varchar(50) DEFAULT NULL COMMENT '创建人',
+  `status` int(11) DEFAULT NULL COMMENT '用户状态',
+  `expire_time` datetime DEFAULT NULL COMMENT '过期时间',
+  `login_count` int(11) DEFAULT NULL COMMENT '登陆次数',
+  `last_login_ip` varchar(20) DEFAULT NULL COMMENT '最后登陆IP',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最后登陆时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
--- src/main/java/com/diyiliu/support/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/diyiliu/support/filter/ExceptionHandlerFilter.java
@@ -1,7 +1,11 @@
 package com.diyiliu.support.filter;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * 处理全局异常
@@ -11,10 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
  * Update: 2018-05-11 10:09
  */
 
-@RestController
+@Controller
 @ControllerAdvice
 public class ExceptionHandlerFilter {
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String noHandlerFound() {
 
+        return "error/404";
+    }
 
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String defaultErrorHandler(Exception e) {
+        e.printStackTrace();
+
+        return "error/500";
+    }
 }
